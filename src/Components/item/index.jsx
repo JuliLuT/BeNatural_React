c36097956a0ac467ada6a3baa7421a255727383a
JuliLuT <tacconijulieta18@gmail.com>
2023-03-20 09:06:07 -0300

    Fixing

--- src/Components/item/index.jsx
+++ src/Components/item/index.jsx
@@ -1,26 +1,32 @@
 import Card from "react-bootstrap/Card";
-import "./item.css";
+import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import "./item.css";
 import { Link } from "react-router-dom";
+
 
 function Item({ product }) {
   return (
-    <Row xs={1} md={2} className="g-4">
-      <Col>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={product.image} />
-          <Card.Body>
-            <Card.Title>{product.name} </Card.Title>
-            <Card.Text>$ {product.price}</Card.Text>
-            <div className="buttons">
-              <button className="normalButton"> <Link to={`/item/${product.id}`}>Detalle</Link></button>
-              <button className="normalButton">Añadir al carrito</button>
-            </div>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
+    <Container>
+      <Row>
+        <Col xs={6} md={4}>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={product.image} />
+            <Card.Body>
+              <Card.Title>{product.name} </Card.Title>
+              <Card.Text>$ {product.price}</Card.Text>
+              <div className="buttons">
+                <button className="normalButton">
+                  <Link to={`/item/${product.id}`}>Detalle</Link>
+                </button>
+                <button className="normalButton">Añadir al carrito</button>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
